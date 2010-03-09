343705e6de2b48a003d4f33d1d5071a89283f997
Aasim <aasim.malladi@gmail.com>
2010-03-09 17:29:11 -0500

    added code for prime_ssn_check from careercup.com

--- cpp/random_prime_ssn.cpp
+++ cpp/random_prime_ssn.cpp
@@ -1,17 +1,62 @@
+// http://www.careercup.com/question?id=311074
+
 #include "../lib/check_prime.hpp"
 
+#include <stdlib.h>
+#include <time.h>
 #include <iostream>
+#include <iomanip>
 
 int main() {
-    int i;
     using namespace aasim_lib;
 
-    while(true) {
-        std::cin >> i;
+    // Generate random 9 digit number
+    // rand() generates a prime number between 0 to RAND_MAX
+    // RAND_MAX is defined to be 2147483647 in the GNU C library
+    
+    srand(time(NULL));
 
-        std::cout << std::boolalpha << check_prime_basic(i) << std::endl;
+    double number = rand() % 899999999 + 100000000;
 
-    }
+    std::cout 
+        << std::fixed
+        << std::setprecision(0)
+        << number 
+        << " is prime? : " 
+        << std::boolalpha 
+        << check_prime_basic(number)
+        << std::endl;
+
+    int first_3_digits = number / 1000000;
+
+    std::cout 
+        << std::fixed 
+        << first_3_digits 
+        << " is prime? : " 
+        << std::boolalpha 
+        << check_prime_basic(first_3_digits)
+        << std::endl;
+
+    int middle_2_digits = (int)number % 1000000;
+    middle_2_digits /= 10000;
+
+    std::cout 
+        << std::fixed 
+        << middle_2_digits 
+        << " is prime? : " 
+        << std::boolalpha 
+        << check_prime_basic(middle_2_digits)
+        << std::endl;
+
+    int last_4_digits = (int)number % 10000;
+    
+    std::cout 
+        << std::fixed 
+        << last_4_digits 
+        << " is prime? : " 
+        << std::boolalpha 
+        << check_prime_basic(last_4_digits)
+        << std::endl;
 
     return 0;
 }
